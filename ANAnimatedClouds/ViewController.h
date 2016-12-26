37837a5307b5e5d20c1f5f2bfb2a48c56b61643e
anna-dev <aabushmeleva@gmail.com>
2016-12-26 12:04:19 +0500

    Initial Commit

---
+++ ANAnimatedClouds/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ANAnimatedClouds
+//
+//  Created by Anna Bushmeleva on 26.12.16.
+//  Copyright © 2016 Your live. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
